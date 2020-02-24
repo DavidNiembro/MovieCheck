6760b0e1d95075803545f605f8801e88ac9a2d4f
David Niembro Scaccianoce <david@niembro.ch>
2020-02-24 09:11:25 +0100

    Add Movie page

---
+++ MovieCheck/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCheck.Models
+{
+    public class Movie
+    {
+        private string title;
+        public Movie(string title)
+        {
+            this.title = title;
+        }
+    }
+}
